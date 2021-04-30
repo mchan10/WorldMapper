bc76a6db593284844825b6fdbfad8f7cc09cbe7f
Michael Chan <michael.chan.3@stonybrook.edu>
2021-04-30 10:50:07 -0400

    added jsTPS and imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import './App.css';
+import React from 'react';
+import {useQuery} from '@apollo/client';
+import { jsTPS } 		from './utils/jsTPS';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
